c8a1995292dfe9a737696a14c54648b12f2c30d3
Augie <augie100801@gmail.com>
2023-01-05 19:22:02 +0700

    Selesai Navigation

--- app/src/main/java/id/ac/ubaya/ta_160419022/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/ta_160419022/view/MainActivity.kt
@@ -3,13 +3,27 @@ package id.ac.ubaya.ta_160419022.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import id.ac.ubaya.ta_160419022.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as
+                NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+//        bottomNav.setupWithNavController(navController)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 }
