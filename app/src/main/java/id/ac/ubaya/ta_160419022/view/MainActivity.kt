9ba1a0ed6313ffe1a4869580a177560af9fad6c6
Augie <augie100801@gmail.com>
2023-01-05 15:43:25 +0700

    implemen camerax

--- app/src/main/java/id/ac/ubaya/ta_160419022/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/ta_160419022/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.ac.ubaya.ta_160419022
+package id.ac.ubaya.ta_160419022.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.ubaya.ta_160419022.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
