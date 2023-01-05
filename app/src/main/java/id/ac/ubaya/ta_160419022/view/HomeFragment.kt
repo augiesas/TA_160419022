9667ded4be53f842782e27df36170b35fff9d7dd
Augie <augie100801@gmail.com>
2023-01-05 18:17:19 +0700

    Camera sudah jadi

--- app/src/main/java/id/ac/ubaya/ta_160419022/view/HomeFragment.kt
+++ app/src/main/java/id/ac/ubaya/ta_160419022/view/HomeFragment.kt
@@ -1,13 +1,46 @@
 package id.ac.ubaya.ta_160419022.view
 
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import com.google.common.util.concurrent.ListenableFuture
 import id.ac.ubaya.ta_160419022.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
+
+    private var preview: Preview ?= null
+    private var imageCapture: ImageCapture?= null
+    private var camera: androidx.camera.core.Camera?= null
+
+    private lateinit var outputDirectory: File
+
+    companion object{
+        private const val TAG = "camera"
+        private const val FILENAME_FORMAT = "yyy-MM-dd-HH-mm-ss"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +48,102 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fabCapture.setOnClickListener {
+            capture()
+        }
+
+        outputDirectory = getOutputDirectory()
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else activity?.filesDir!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(permissionGranted()){
+            startCamera()
+        }
+        else {
+            ActivityCompat.requestPermissions(context as Activity, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+
+    private fun permissionGranted() = REQUIRED_PERMISSIONS.all{
+        ContextCompat.checkSelfPermission(
+            requireActivity().baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> = ProcessCameraProvider.getInstance(
+            requireContext()
+        )
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+            preview = Preview.Builder().build()
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                preview?.setSurfaceProvider(camView.createSurfaceProvider(camera?.cameraInfo))
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, context?.let { ContextCompat.getMainExecutor(it) })
+    }
+
+    private fun capture() {
+        // Saat capture ini masih save
+        // harus dirubah biar foto yang di dapet bukan ke save ta[i ke upload
+        // ==================================================================================================================================
+
+        val imageCapture: ImageCapture = imageCapture?:return
+
+        // Create file
+        val file = File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT, Locale.JAPAN).format(System.currentTimeMillis())+".jpg")
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(requireContext()), object: ImageCapture.OnImageSavedCallback{
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val savedUri: Uri = Uri.fromFile(file)
+
+                Toast.makeText(context, "Photo captured on: \n\n $savedUri", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                Toast.makeText(context, "Failed to take a photo", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS){
+            if (permissionGranted()){
+                startCamera()
+            } else {
+                Toast.makeText(context, "This application need permission to access camera", Toast.LENGTH_LONG).show()
+                requireActivity().finish()
+            }
+        }
     }
 }
--- app/src/main/java/id/ac/ubaya/ta_160419022/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/ta_160419022/view/MainActivity.kt
@@ -2,11 +2,14 @@ package id.ac.ubaya.ta_160419022.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import id.ac.ubaya.ta_160419022.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
